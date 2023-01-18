74e8db292736f49f0b5f336e5d200a1f33515619
Christian Tello <christian.cate@gmail.com>
2023-01-18 11:23:11 -0500

    implementation for the page leercapitulo.com

--- src/test/java/com/olva/scraptest/MainTest.java
+++ src/test/java/com/olva/scraptest/MainTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.olva.scraptest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.text.DecimalFormat;
 import java.util.Iterator;
 import java.util.List;
@@ -48,55 +44,38 @@ public class MainTest {
             e.printStackTrace();
         }
         try {
-            for (int i = 0; i < 1; i++) {
+            for (int i = 97; i <= 102; i++) {
                 String fileName = "C:\\Users\\chris\\Desktop\\Saint Seiya\\Next Dimension\\";
-                String srcImg = "", disabled;
-                System.out.println("obteniendo pagina: " + "https://sites.google.com/site/saintseiyand/capitulos/saint-seiya-next-dimension-" + FORMAT_NUMBER.format(i));
-//                Document main = Jsoup.connect("https://sites.google.com/site/saintseiyand/capitulos/saint-seiya-next-dimension-" + FORMAT_NUMBER.format(i)).get();
-                Document main = Jsoup.connect("https://es.novelcool.com/chapter/Saint-Seiya-Next-Dimension-Cap-tulo-96/5739868-2.html").get();
-                System.out.println("obtenido");
-//                Elements iframe = main.select(".sites-embed-content.sites-embed-type-punch iframe");
-                Elements imagenes = main.select(".site-content.mangaread-img.pic_box img");
-//                String src = iframe.attr("src");
-                System.out.println("cantidad de imagenes: " + imagenes.size());
-                Iterator<Element> elements = imagenes.iterator();
-//                while (elements.hasNext()) {                    
-//                    Element element = elements.next();
-//                    element.
-//                }
-//                System.out.println("obteniendo frame: " + src);
-//                ghostDriver.get(src);
-//                System.out.println("obtenido");
-//                int nombreImg = 0;
-//                do {
-//                    Document frame = Jsoup.parse(ghostDriver.getPageSource());
-//                    Elements imagenes = frame.select(".punch-viewer-svgpage-svgcontainer image");
-//                    Element imagen = imagenes.last();
-//                    String imageName = fileName + "\\" + FORMAT_NUMBER.format(i) + "-" + FORMAT_NUMBER.format(nombreImg++) + ".jpg";
-//                    if (imagen != null) {
-//                        srcImg = imagen.attr("xlink:href");
-//                        System.out.println(srcImg);
-//                        BufferedImage image = null;
-//                        try {
-//                            URL url = new URL(srcImg);
-//                            image = ImageIO.read(url);
-//                            ImageIO.write(image, "jpg", new File(imageName));
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }else{
-//                        try {
-//                            ImageIO.write(imageNotFound, "jpg", new File(imageName));
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                    List<WebElement> botones = ghostDriver.findElements(By.cssSelector(".goog-inline-block.goog-flat-button"));
-//                    WebElement boton = botones.get(3);
-//                    disabled = boton.getAttribute("aria-disabled");
-//                    System.out.println(disabled);
-//                    boton.click();
-//                } while (!Boolean.valueOf(disabled));
+                String disabled;
+                for (int j = 1; j < 2; j++) {
+                    System.out.println("obteniendo pagina: " + "https://www.leercapitulo.com/read/saint-seiya-next-dimension/" + FORMAT_NUMBER.format(i) + "/#" + j);
+                    Document main = Jsoup.connect("https://www.leercapitulo.com/read/saint-seiya-next-dimension/" + FORMAT_NUMBER.format(i) + "/#" + j)
+                            .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
+                            .get();
+                    System.out.println("obtenido");
+                    int nombreImg = 0;
+                    Elements imageElements = main.select("#arraydata");
+                    String imageUrls = imageElements.text();
+                    System.out.println("imagenes:");
+                    System.out.println(imageUrls);
+                    if(imageUrls != null && !imageUrls.isEmpty()){
+                        String[] images = imageUrls.split(",");
+                        for (int k = 0; k < images.length; k++) {
+                            String srcImg = images[k];
+                            String imageName = fileName + "\\" + FORMAT_NUMBER.format(i) + "-" + FORMAT_NUMBER.format(nombreImg++) + ".jpg";
+                            System.out.println(srcImg);
+                            try {
+                                URL url = new URL(srcImg);
+                                URLConnection urlConnection = url.openConnection();
+                                urlConnection.setRequestProperty("User-Agent", "NING/1.0");
+                                BufferedImage imageB = ImageIO.read(urlConnection.getInputStream());
+                                ImageIO.write(imageB, "jpg", new File(imageName));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
             }
         } catch (IOException ex) {
             Logger.getLogger(MainTest.class.getName()).log(Level.SEVERE, null, ex);
